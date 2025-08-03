b5943336339ecb48bd344b914d49a44d9626b4cf
Alexander Bell <contact@alexanderbell.info>
2025-08-03 13:24:30 -0700

    Use string for empty alt tag

--- src/app/components/ListItem.tsx
+++ src/app/components/ListItem.tsx
@@ -12,7 +12,7 @@ export default function ListItem({entry}: {entry: Pokémon}) {
     return (
         <div style={{ maxWidth: '768px', marginInline: 'auto', borderRadius: '64px 16px 16px 64px', backgroundColor: '#f2f2f2', backgroundImage: 'linear-gradient(100deg, #e8e8e8, #fcfcfc)', backgroundRepeat: 'no-repeat', gap: '0 24px', display: 'grid',gridTemplateColumns: '128px 1fr 128px', gridTemplateRows: 'repeat(2, 64px)', justifyContent: 'start', alignItems: 'center', boxShadow: '0 2px 5px rgba(0 0 0 / 15%)', position: 'relative', overflow: 'hidden' }} key={entry.entry_number} className={versionClass}>
             <div className="sprite" style={{ gridRow: '1 / 2', alignSelf: 'flex-start', padding: '32px', backgroundImage: 'radial-gradient(rgba(255 255 255 / 60%) 60%, rgba(31 31 31 / 8%) 60%, rgba(0 0 0 / 8%) 70%, transparent 71%)' }}>
-                <Image src={} alt={} height={64} width={64} style={{ color: 'transparent', display: 'block' }} />
+                <Image src={} alt="" height={64} width={64} style={{ color: 'transparent', display: 'block' }} />
             </div>
             <div className="copy" style={{ justifySelf: 'auto', display: 'flex', gap: '24px',alignItems: 'baseline' }}>
                 <h3 style={{ fontSize: '36px', lineHeight: 1.333 }}>{capitalizeFirst(entry.pokemon_species.name)}</h3>
