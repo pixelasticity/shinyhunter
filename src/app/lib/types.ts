365023bcf9095c725b9f26d594694e7f9df9f8f0
Alexander Bell <contact@alexanderbell.info>
2025-08-21 22:33:25 -0700

    Update types.ts

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -7,13 +7,20 @@ export type PokemonEntry = {
 };
 
 export type PokemonData = {
-  types: Array<{ type: { name: string } }>;
+  types: Array<{ type: { name: string; url: string } }>; // Added url here
   id: number;
+  name?: string;
+  species?: {
+    name: string;
+    url: string;
+  };
 };
 
 export type SpeciesData = {
   color: { name: string };
   id: number;
+  name?: string;
+  names: Name[];
 };
 
 export type PokemonState = 'none' | 'caught' | 'shiny';
@@ -25,9 +32,18 @@ export interface PokemonSpriteInfo {
   altText: string;
 }
 
+export type Name = {
+  language: {
+    name: string;
+    url: string;
+  };
+  name: string;
+};
+
 export interface PokemonInfo {
   entryNumber: number;
   name: string;
+  names: Name[];
   speciesUrl?: string | null;
   nationalId?: number | undefined;
   styles?: { [key: string]: string };
@@ -36,10 +52,19 @@ export interface PokemonInfo {
 export type Types = {
   type: {
     name: string;
+    url?: string; // Make url optional here as it might not always be present
   };
+};
+
+export type TypeData = {
+  id: number;
+  name: string;
+  names: Name[];
+  url?: string; // Add url here as it's used in useBatchedPokemonData
 };
 
 export interface StatsProps {
   pokemonEntries: PokemonEntry[];
+  pokédex: string;
   className?: string;
 }
