40663e527e9602840c2ec993508abeb104fb4645
Alexander Bell <contact@alexanderbell.info>
2025-08-21 23:54:12 -0700

    Use translations

--- src/app/components/PokemonRow.tsx
+++ src/app/components/PokemonRow.tsx
@@ -2,25 +2,32 @@
 
 import Image from 'next/image';
 import { useMemo, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './list.module.css';
 import Checkbox from './Checkbox';
 import { usePokemonState } from '../hooks/usePokemonState';
 import { useIntersectionObserver } from '../hooks/useIntersectionObserver';
 import { capitalizeFirst } from '../lib/utils';
-import { PokemonEntry, Types } from '../lib/types';
+import { PokemonEntry, Name, Types, PokemonData, SpeciesData, TypeData } from '../lib/types';
+
+type BatchedData = {
+  getPokemonData: (name: string) => PokemonData | undefined;
+  getSpeciesData: (name: string) => SpeciesData | undefined;
+  getTypeData: (name: string) => TypeData | undefined;
+};
 
 // Component to display individual Pokemon data with batched data
 export default function PokemonRow({
   entry,
   getPokemonData,
   getSpeciesData,
+  getTypeData,
   batchLoading,
 }: {
   entry: PokemonEntry;
-  getPokemonData: (name: string) => any;
-  getSpeciesData: (name: string) => any;
   batchLoading: boolean;
-}) {
+} & BatchedData) {
+  const { t, i18n } = useTranslation();
   const ref = useRef<HTMLDivElement | null>(null);
   const options = useMemo(() => ({ rootMargin: '200px' }), []);
   const isIntersecting = useIntersectionObserver(ref, options);
@@ -29,7 +36,6 @@ export default function PokemonRow({
   const pokemonData = getPokemonData(pokemonName);
   const speciesData = getSpeciesData(pokemonName);
   const nationalId = pokemonData?.id;
-
   // Use the custom hook for Pokemon state management
   const {
     spriteInfo,
@@ -38,19 +44,33 @@ export default function PokemonRow({
   } = usePokemonState({
     entryNumber: entry.entry_number,
     name: pokemonName,
+    names: speciesData?.names || [],
     nationalId,
-    styles
   });
 
   // Get color from the species data
-  const color = speciesData?.color?.name || null;
+  const color = speciesData?.color?.name || '';
   const types = pokemonData?.types?.map((type: Types) => type.type.name) || [];
+  const names = speciesData?.names || [];
+  const translatedName = useMemo(() => {
+    const nameData = names.find((name: Name) => name.language.name === i18n.language);
+    return nameData?.name || pokemonName; // Fallback to the default name
+  }, [names, i18n.language, pokemonName]);
+
+  const translatedTypes = useMemo(() => {
+    return types.map((typeName: string) => {
+      const typeData = getTypeData(typeName);
+      const translatedType = typeData?.names.find((name: Name) => name.language.name === i18n.language);
+      return translatedType?.name || capitalizeFirst(typeName);
+    });
+  }, [types, getTypeData, i18n.language]);
+
   const isDataMissing = !pokemonData;
 
   return (
     <div ref={ref} key={entry.entry_number} className={`${styles['pokemon']} ${styles[color]}`}>
       <div className={styles.checkbox} >
-        <Checkbox id={nationalId} name={pokemonName} />
+        <Checkbox id={nationalId || 0} name={pokemonName} />
       </div>
       <div className={styles.sprite}>
         {isIntersecting ? (
@@ -66,31 +86,37 @@ export default function PokemonRow({
         )}
       </div>
       <div className={styles.info}>
-        <h3 className={styles.name}>{capitalizeFirst(entry.pokemon_species.name)}</h3>
+        <h3 className={styles.name}>{batchLoading ? (
+          <span>{t('loading')}</span>
+        ) : isDataMissing ? (
+          <span>{t('unknown')}</span>
+        ) : (
+          <span lang={i18n.language}>{translatedName}</span>
+        )}</h3>
         <span className={styles.number}>
-          <span className={styles.hash}><span className="visually-hidden">Number</span><span role='presentation'>#</span></span>{formattedEntryNumber}
+          <span className={styles.hash}><span className="visually-hidden">{t('number')}</span><span role='presentation'>#</span></span>{formattedEntryNumber}
         </span>
       </div>
       <span className={styles.types} aria-label={
         batchLoading
-          ? "The types are loading"
+          ? t('loading')
           : isDataMissing
-            ? "Types unavailable"
-            : `Types: ${types.map((type: string) => capitalizeFirst(type)).join(', ')}`
+            ? t('unknown')
+            : `${t('types')}: ${translatedTypes.join(', ')}`
       }>
         {batchLoading ? (
-          <span className={styles['type']} role="term">Loading&hellip;</span>
+          <span className={styles['type']} role="term">{t('loading')}</span>
         ) : isDataMissing ? (
-          <span className={styles['type']} role="term" aria-label="Missing type">Unknown</span>
+          <span className={styles['type']} role="term" aria-label="Missing type">{t('unknown')}</span>
         ) : (
-          types.map((type: string) => (
+          translatedTypes.map((type: string, index: number) => (
             <span
               key={type}
-              className={`${styles['type']} ${styles[type]}`}
+              className={`${styles['type']} ${styles[types[index]]}`}
               role="term"
-              aria-label={`${capitalizeFirst(type)} type`}
+              aria-label={`${type} type`}
             >
-              {capitalizeFirst(type)}
+              {type}
             </span>
           ))
         )}
